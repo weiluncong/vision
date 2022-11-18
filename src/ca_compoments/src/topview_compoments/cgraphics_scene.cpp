19e68735e433f36661f9b4181a1407bcd48e75ed
wangkuan <63118@any3.com>
2022-11-18 10:16:18 +0800

    完善graphicsScene功能模块函数

--- src/ca_compoments/src/topview_compoments/cgraphics_scene.cpp
+++ src/ca_compoments/src/topview_compoments/cgraphics_scene.cpp
@@ -28,6 +28,51 @@ void CGraphicsScene::CreateMenu()
 void CGraphicsScene::wheelEvent(QGraphicsSceneWheelEvent *event)
 {
     CVehicleBackGroundItem *back_item = static_cast<CVehicleBackGroundItem *>(this->items().back());
+    const QPointF cur_pos = back_item->PointSceneInItem(event->scenePos());
+    double x_axis_min = back_item->x_axis_min_;
+    double x_axis_max = back_item->x_axis_max_;
+    double y_axis_min = back_item->y_axis_min_;
+    double y_axis_max = back_item->y_axis_max_;
+    const double factor = 1.05;
+    const double x_central = cur_pos.x();
+    const double y_central = cur_pos.y();
+    double bottom_offset = 0, top_offset = 0, left_offset = 0, right_offset = 0;
+    if (event->delta() > 0)
+    {
+        bottom_offset = 1.0 / factor * (y_central - y_axis_min);
+        top_offset = 1.0 / factor * (y_axis_max - y_central);
+        left_offset = 1.0 / factor * (x_central - x_axis_min);
+        right_offset = 1.0 / factor * (x_axis_max - x_central);
+    }
+    else
+    {
+        bottom_offset = 1.0 * factor * (y_central - y_axis_min);
+        top_offset = 1.0 * factor * (y_axis_max - y_central);
+        left_offset = 1.0 * factor * (x_central - x_axis_min);
+        right_offset = 1.0 * factor * (x_axis_max - x_central);
+    }
+    switch (zoom_flag_)
+    {
+    case cZoomMode::ZoomX:
+        back_item->x_axis_min_ = x_central - left_offset;
+        back_item->x_axis_max_ = x_central + right_offset;
+        this->update();
+        break;
+    case cZoomMode::ZoomY:
+        back_item->y_axis_min_ = y_central - bottom_offset;
+        back_item->y_axis_max_ = y_central + top_offset;
+        this->update();
+        break;
+    case cZoomMode::ZoomXY:
+        back_item->x_axis_min_ = x_central - left_offset;
+        back_item->x_axis_max_ = x_central + right_offset;
+        back_item->y_axis_min_ = y_central - bottom_offset;
+        back_item->y_axis_max_ = y_central + top_offset;
+        this->update();
+        break;
+    default:
+        break;
+    }
 }
 
 /** @brief 鼠标释放按键事件*/
@@ -54,7 +99,19 @@ void CGraphicsScene::mousePressEvent(QGraphicsSceneMouseEvent *event)
 /** @brief 鼠标移动事件*/
 void CGraphicsScene::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
 {
-    const QPointF p = event->scenePos();
+    CVehicleBackGroundItem *back_item = static_cast<CVehicleBackGroundItem *>(this->items().back());
+    const QPointF cur_pos = event->scenePos();
+    QPointF cur_val = back_item->PointSceneInItem(cur_pos);
+    if (is_press_)
+    {
+        QPointF last_val = back_item->PointSceneInItem(last_point_);
+        last_point_ = cur_pos;
+        back_item->x_axis_min_ -= (cur_val - last_val).x() * 0.6;
+        back_item->x_axis_max_ -= (cur_val - last_val).x() * 0.6;
+        back_item->y_axis_min_ -= (cur_val - last_val).y() * 0.6;
+        back_item->y_axis_max_ -= (cur_val - last_val).y() * 0.6;
+        this->update();
+    }
 
     QGraphicsScene::mouseMoveEvent(event);
 }
